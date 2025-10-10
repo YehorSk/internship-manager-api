81ebd8852869bc35e115637d5733d1ea8298d23e
YehorSk <95549799+YehorSk@users.noreply.github.com>
2025-10-10 12:48:35 +0200

    Login functionality IM-57, IM-56, IM-55
    added Resources

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,4 +67,9 @@ class User extends Authenticatable
         return $this->belongsToMany(Role::class, 'role_user', 'user_id', 'role_id');
     }
 
+    public function hasRole($role)
+    {
+        return $this->roles()->where('name', $role)->exists();
+    }
+
 }
