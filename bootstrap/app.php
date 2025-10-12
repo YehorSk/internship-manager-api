eac5197150ec5f9a5db5979fff0d7d4087fc13ff
Petr <kpa80@yandex.ru>
2025-10-12 14:19:26 +0200

    Fix scheduled task to clear authentication reset tokens every two hours

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
     })
+    ->withSchedule(function (Illuminate\Console\Scheduling\Schedule $schedule) {
+        $schedule->command('auth:clear-resets')->everyTwoHours();
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
--- config/schedule.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Console\Scheduling\Schedule;
-
-return function (Schedule $schedule) {
-    $schedule->command('auth:clear-resets')->everyTwoHours();
-};
-
