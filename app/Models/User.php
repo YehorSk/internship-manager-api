beea1631b69e589dae41de23b64cd3ff78e66b76
Petr <kpa80@yandex.ru>
2025-10-26 14:01:26 +0100

    Remove unnecessary comments from role checking methods in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,14 +71,11 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function hasRole($role)
     {
-        // Явно префиксуем имя колонки таблицей roles, чтобы избежать неоднозначности
         return $this->roles()->where('roles.name', $role)->exists();
     }
 
     public function hasRoleId(int $roleId): bool
     {
-        // Явно префиксуем имя колонки таблицей roles, иначе при join с pivot-таблицей
-        // может возникать "Column 'id' in WHERE is ambiguous" (если в pivot тоже есть id)
         return $this->roles()->where('roles.id', $roleId)->exists();
     }
 
