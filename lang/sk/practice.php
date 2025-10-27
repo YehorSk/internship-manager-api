0c198c334024e8bbbd9315114b543930f982c6e2
Petr <kpa80@yandex.ru>
2025-10-27 23:30:18 +0100

    Add description for new endpoint and language constants

--- lang/sk/practice.php
+++ lang/sk/practice.php
@@ -6,4 +6,8 @@
         'updated_successfully' => 'Praxa bola úspešne aktualizovaná.',
         'delete_not_allowed' => 'Túto prax nie je možné odstrániť.',
         'deleted_successfully' => 'Praxa bola úspešne odstránená.',
+        'agreement_approval_requested_successfully' => 'Žiadosť o schválenie dohody o praxi bola úspešne odoslaná.',
+        'agreement_confirmation_requested' => 'Žiadosť o potvrdenie dohody o praxi bola úspešne odoslaná.',
+        'agreement_reconfirmation_requested' => 'Žiadosť o opätovné potvrdenie dohody o praxi bola úspešne odoslaná.',
+        'request_approval_not_allowed' => 'Žiadosť o schválenie dohody o praxi nie je povolená.',
     ];
