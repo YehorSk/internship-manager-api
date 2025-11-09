21b65ec1c7a9436183aba750c4c186306b39cebf
YehorSk <95549799+YehorSk@users.noreply.github.com>
2025-11-09 10:49:13 +0100

    401 localization

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
+use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -28,5 +29,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('auth:clear-resets')->everyTwoHours();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AuthenticationException $e, $request) {
+            if ($request->expectsJson() || $request->is('api/*')) {
+                return response()->json([
+                    'message' => __('auth.unauthenticated'),
+                ], 401);
+            }
+        });
     })->create();
