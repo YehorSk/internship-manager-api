e09643e64b83d9f40367ab5e0e7e7b515d370217
YehorSk <95549799+YehorSk@users.noreply.github.com>
2025-11-13 15:04:05 +0100

    mail templates fix

--- resources/views/emails/send_password.blade.php
+++ resources/views/emails/send_password.blade.php
@@ -1,9 +1,9 @@
 <x-mail::message>
-    # {{ __('emails.account_created_greeting', ['name' => $user->name]) }}
+# {{ __('mail.account_created_greeting', ['name' => $user->name]) }}
 
-    {{ __('emails.account_created_message') }}
+{{ __('mail.account_created_message') }}
 
-    **{{ __('emails.temporary_password', ['password' => $password]) }}**
+**{{ __('mail.temporary_password', ['password' => $password]) }}**
 
-    {{ __('emails.account_created_instruction') }}
+{{ __('mail.account_created_instruction') }}
 </x-mail::message>
