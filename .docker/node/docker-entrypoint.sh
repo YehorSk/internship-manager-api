4b135ab6949af0dd55dc932ff13520f70f1569df
Petr <kpa80@yandex.ru>
2025-12-01 10:18:27 +0100

    Adjust ownership in docker configuration

--- .docker/node/docker-entrypoint.sh
+++ .docker/node/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
-
+for dir in /var/www/*; do
+  chown -R 1000:1000 "$dir"
+done
 cd /var/www/internship-manager-web
 npm install
 npm run dev
--- .docker/php-fpm/docker-entrypoint.sh
+++ .docker/php-fpm/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 for dir in /var/www/*; do
-  chown -R www-data:www-data "$dir"
+  chown -R 1000:1000 "$dir"
 done
 chmod -R 775 /var/www/internship-manager-api/storage /var/www/internship-manager-api/bootstrap/cache
 exec php-fpm -F
